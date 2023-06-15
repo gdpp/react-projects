4701132ffea293401e0908bdeb6451d4f1d3d776
Gustavo.Perez <gustavo.perez.231191@gmail.com>
2023-06-15 14:49:12 -0600

    formik project in progress

--- react-formik-project/src/components/YoutubeForm.jsx
+++ react-formik-project/src/components/YoutubeForm.jsx
@@ -1,11 +1,23 @@
-import {Formik, Form} from 'formik';
+/* eslint-disable react/prop-types */
+import {Formik, Form, Field, ErrorMessage, FieldArray} from 'formik';
 import * as Yup from 'yup';
+
+// Components
+import TextError from './TextError';
 
 const YoutubeForm = () => {
   const initialValues = {
     name: '',
     email: '',
-    channel: ''
+    channel: '',
+    comments: '',
+    address: '',
+    social: {
+      facebook: '',
+      twitter: ''
+    },
+    phoneNumbers: ['', ''],
+    phNumbers: ['']
   }
 
   const onSubmit = values => {
@@ -30,27 +42,81 @@ const YoutubeForm = () => {
       <Form>
         <div className='form-control'>
           <label htmlFor="name">Name</label>
-          <input type="text" id='name' name='name' {...formik.getFieldProps('name')}/>
-          { formik.touched.name && formik.errors.name ?
-            <div className='error'>{formik.errors.name}</div> :
-            null
-          }
+          <Field type="text" id='name' name='name' />
+          <ErrorMessage name='name' component={TextError}/>
         </div>
         <div className='form-control'>
           <label htmlFor="email">E-mail</label>
-          <input type="email" id='email' name='email' {...formik.getFieldProps('email')}/>
-          { formik.touched.email && formik.errors.email ?
-            <div className='error'>{formik.errors.email}</div> :
-            null
-          }
+          <Field type="email" id='email' name='email' />
+          <ErrorMessage name='email'>
+            {
+              errMsg => <div className='error'>{errMsg}</div> 
+            }
+          </ErrorMessage>
         </div>
         <div className='form-control'>
           <label htmlFor="channel">Channel</label>
-          <input type="text" id='channel' name='channel' {...formik.getFieldProps('channel')}/>
-          { formik.touched.channel && formik.errors.channel ?
-            <div className='error'>{formik.errors.channel}</div> :
-            null
-          }
+          <Field type="text" id='channel' name='channel' placeholder='Youtube channel name'/>
+          <ErrorMessage name='channel'/>
+        </div>
+        <div className="form-control">
+          <label htmlFor="comments">Comments</label>
+          <Field as='textarea' id='comments' name='comments'/>
+        </div>
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <Field name='address'>
+            {
+              (props) => {
+                console.log(props);
+                const {field, form, meta} = props;
+                return <div>
+                  <input type='text' id='address' {...field} />
+                  {meta.touched && meta.error ? <div>{meta.error}</div> : null }
+                </div>
+              }
+            }
+          </Field>
+        </div>
+        <div className="form-control">
+          <label htmlFor="facebook">Facebook profile</label>
+          <Field type='text' id='facebook' name='social.facebook' />
+        </div>
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter profile</label>
+          <Field type='text' id='twitter' name='social.twitter' />
+        </div>
+        <div className="form-control">
+          <label htmlFor="pphone">Primary phone</label>
+          <Field type='text' id='pphone' name='phoneNumbers[0]' />
+        </div>
+        <div className="form-control">
+          <label htmlFor="sphone">Secondary phone</label>
+          <Field type='text' id='sphone' name='phoneNumbers[1]' />
+        </div>
+        <div className="form-control">
+          <label htmlFor="listPhone">List of phone numbers</label>
+          <FieldArray name='phNumbers'>
+            {
+              (fieldArrayProps) => {
+                const {push, remove, form} = fieldArrayProps;
+                const {values} = form;
+                const {phNumbers} = values;
+
+                return <div>
+                  {
+                    phNumbers.map((phone, idx) => (
+                      <div key='idx'>
+                        <Field name={`phNumbers[${idx}]`}/>
+                        <button type='button' onClick={() => remove(idx)}>-</button>
+                        <button type='button' onClick={() => push('')}>+</button>
+                      </div>
+                    ))
+                  }
+                </div>;
+              }
+            }
+          </FieldArray>
         </div>
         <button type='submit'>Submit</button>
       </Form>
