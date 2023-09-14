18a65d31842b6755602675938cfa574dd7595cbb
Gustavo Perez <gustavo.perez.231191@gmail.com>
2023-09-14 17:43:50 -0600

    Detail hotel

--- hotel-app/src/components/HotelDetails.jsx
+++ hotel-app/src/components/HotelDetails.jsx
@@ -2,3 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useRoute } from "wouter";
 
 import { Typography, Card, CardMedia, CardContent, CardActions } from "@mui/material";
+
+const fetchHotel = () => {
+  
+}
